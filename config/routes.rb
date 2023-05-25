eccb956b99f168a9ac9916d62bb59a493a955fde
Wagner Abrantes <wagner.dev1337@gmail.com>
2023-05-25 12:21:44 -0300

    auth configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  use_doorkeeper
+
+  namespace :api do
+    namespace :v1 do
+      resources :profiles
+      resources :users
+
+      get "/me",   to: "credentials#me"
+      get "/fast", to: "fast#index"
+    end
+  end
+
+  devise_for :users
+
+  root to: "home#index"
 end
